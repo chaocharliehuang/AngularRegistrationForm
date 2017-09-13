c155f424c19f27c042ebfff3fd447ae32f8d0064
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-09-13 16:19:30 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  user = new User();
+  lastUser;
+  success = false;
+  onSubmit() {
+    this.lastUser = this.user;
+    this.user = new User();
+    this.success = true;
+  }
+}
